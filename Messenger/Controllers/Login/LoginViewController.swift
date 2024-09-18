516a753a7e55c9846b843d366efcba9f2b15ad81
Yvan Gauthier <y.gauthier04@icloud.com>
2024-09-18 18:16:19 +0200

    Mise en place du button Login and initialisation de Firebase

--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -8,22 +8,123 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        return scrollView
+    }()
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "Logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+        
+    }()
+    
+    private let emailField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "Email address ..."
+        field.leftView = UIView(frame: CGRect(x:0, y: 0, width : 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        return field
+    }()
+    private let passwordField: UITextField = {
+        let field = UITextField()
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.returnKeyType = .continue
+        field.layer.cornerRadius = 12
+        field.layer.borderWidth = 1
+        field.layer.borderColor = UIColor.lightGray.cgColor
+        field.placeholder = "password ..."
+        field.leftView = UIView(frame: CGRect(x:0, y: 0, width : 5, height: 0))
+        field.leftViewMode = .always
+        field.backgroundColor = .white
+        field.isSecureTextEntry = true
+        return field
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login In", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 12
+        button.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold )
+        return button
+            
+        
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Log in"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTabRegister))
+        
+        // Add subViews
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.frame = view.bounds
+        
+        let size = scrollView.width/3
+        imageView.frame = CGRect(x: (view.width-size)/2,
+                                 y: 20,
+                                 width: size,
+                                 height: size)
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bottom+10,
+                                  width: scrollView.width-60,
+                                 height: 52)
+      passwordField.frame = CGRect(x: 30,
+                                   y: emailField.bottom+10,
+                                  width: scrollView.width-60,
+                                 height: 52)
+        loginButton.frame = CGRect(x: 30,
+                                   y: passwordField.bottom+10,
+                                    width: scrollView.width-60,
+                                   height: 52)
+    }
+    @objc private func didTapLogin() {
+        guard let email = emailField.text, let password = passwordField.text,
+              !email.isEmpty, !password.isEmpty, password.count >= 6 else {
+            alertUserLoginError()
+            return
+        }
+       // Firebase Login
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func alertUserLoginError() {
+        let alert = UIAlertController(title: "Woops",
+                                      message: "Please enter all information to login",
+                                      preferredStyle: .alert )
+        alert.addAction(UIAlertAction(title: "Dismiss",
+                                      style: .cancel, handler: nil))
+        present(alert, animated: true)
     }
-    */
-
+    
+    @objc private func didTabRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create Account"
+        navigationController?.pushViewController(vc, animated: true  )
+    }
 }
